734a8e1921f97cd049dfe227c143972dca889d08
Aziz Faozi <cahalapisan@gmail.com>
2017-11-28 20:57:12 +0700

    first commit

---
+++ app/src/main/java/org/labseni/sanchez/daftar.java
@@ -0,0 +1,13 @@
+package org.labseni.sanchez;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class daftar extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar);
+    }
+}
---
+++ app/src/main/java/org/labseni/sanchez/menuutama.java
@@ -0,0 +1,13 @@
+package org.labseni.sanchez;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menuutama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menuutama);
+    }
+}
